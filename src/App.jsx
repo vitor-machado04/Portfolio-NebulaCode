c32a158381fa45d397a7f3575e70e1ac2c7bd214
unknown <viniciussieben7@gmail.com>
2023-07-19 21:29:39 -0300

    Adicionado gradiente no fundo da página

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
+      <div class="bg-gradient-to-b from-[#1a0c2d] to-[#0d051b] w-full h-full">
         <About />
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
-      
+      </div>
         </div>
       </div>
     </BrowserRouter>
